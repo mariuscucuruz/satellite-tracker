be9c09805791a89485e49bb675fab053c5f2e475
Marius Cucuruz (Mint) <marius.cucuruz@gmail.com>
2020-06-24 19:09:36 +0100

    grouped `satellite` calls from `subscribers`

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,21 +9,22 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
 $router->group([
     'prefix' => 'satellite',
     'namespace' => '\App\Http\Controllers'
-],
-    function () use ($router) {
-
+], function () use ($router) {
         $router->get('/{noradId:[0-9]+}', 'SatelliteController@findNoradId');
-
-        $router->get('/{noradId:[0-9]+}/predictions', 'SatelliteController@getPredictions');
-
-        $router->post('/subscribe', 'SubscriberController@submit');
-        $router->put('/subscribe', 'SubscriberController@remove');
+        $router->get('/{noradId:[0-9]+}/predictions/{lat}/{lng}[/{alt:[0-9]+}]', 'SatelliteController@getPredictions');
     });
 
-
+$router->group([
+    'prefix' => 'subscribe',
+    'namespace' => '\App\Http\Controllers'
+], function () use ($router) {
+        $router->post('/', 'SubscriberController@subscribe');
+        $router->put('/', 'SubscriberController@remove');
+    });
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
